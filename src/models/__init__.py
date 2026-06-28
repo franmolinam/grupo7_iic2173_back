c0585551053cda687883e18a4e09b6312715129c
Francisca Molina <francisca.molina@uc.cl>
2026-06-28 04:45:25 -0400

    Implementacion de suscripciones

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,9 @@
 # exporto todos los modelos para que puedan ser importados después desde otros módulos
-from src.models.package import Package
-from src.models.city_connection import CityConnection
-from src.models.package_event import PackageEvent
-from src.models.shipment_request import ShipmentRequest
-from src.models.payment import Payment
-from src.models.routing_jobs import RoutingJob
-from src.models.branch_config import BranchConfig
-from src.models.subscription import Subscription, SubscriptionPackage
+from src.models.package import Package # noqa: F401
+from src.models.city_connection import CityConnection # noqa: F401
+from src.models.package_event import PackageEvent # noqa: F401
+from src.models.shipment_request import ShipmentRequest # noqa: F401
+from src.models.payment import Payment # noqa: F401
+from src.models.routing_jobs import RoutingJob # noqa: F401
+from src.models.branch_config import BranchConfig # noqa: F401
+from src.models.subscription import Subscription, SubscriptionPackage # noqa: F401
